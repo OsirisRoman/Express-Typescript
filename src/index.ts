c85a68ba4d4c9f37b772389dc1f851f5758d170a
Osiris Roman <osiris.roman@ulinksolution.com>
2021-09-07 08:16:11 -0500

    initial server config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hi There!");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+        <div>
+            <h1>Hi There!</h1>
+        </div>
+    `);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
